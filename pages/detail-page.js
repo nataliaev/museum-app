07d7a1d0e0bbeffac3d0e939e16af53bc6a5f8f3
nataliaev <nataliiaevchenko@gmail.com>
2019-07-02 19:31:51 +0200

    Get input

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,3 +1,7 @@
 function submitComment() {
-    console.log('Hello!');
+    const inputField = document.getElementById('name');
+    const name = inputField.value;
+    const inputMessage = document.getElementById('msg');
+    const msg = inputMessage.value;
+    console.log(msg);
 }
