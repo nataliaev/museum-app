9682c3bd6f416b6e4c14048468ae56ccaaebb850
nataliaev <nataliiaevchenko@gmail.com>
2019-07-03 14:12:52 +0200

    add classList

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,7 +1,17 @@
+// add event handler
 function submitComment() {
+    // gather data
     const inputField = document.getElementById('name');
     const name = inputField.value;
     const inputMessage = document.getElementById('msg');
     const msg = inputMessage.value;
-    console.log(msg);
+    // create the elements you need
+    const comment = document.createElement('section');
+    const h3 = document.createElement('h3');
+    const p = document.createElement('p');
+    //reassigning properties
+    h3.innerHTML = `${name} said:`;
+    p.innerHTML = msg;
+    comment.classList = 'comment';
+    console.log(comment, h3, p);
 }
